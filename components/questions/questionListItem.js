538f1fd9a3ec03b68516086512bcbbd0f2063ba1
TKHAN VIN NHUIEN <gatorfl2014@gmail.com>
2023-08-22 11:24:11 -0400

    edit form input styling

--- components/questions/questionListItem.js
+++ components/questions/questionListItem.js
@@ -6,15 +6,15 @@ export default function QuestionListItem(props) {
   const [open, setOpen] = useState(false);
 
   return (
-    <li onClick={() => (open ? setOpen(false) : setOpen(true))} key={question} className="py-5">
+    <li onClick={() => (open ? setOpen(false) : setOpen(true))} key={question} className="py-5 relative">
       <div className="flex justify-between gap-x-6 ">
         <p>{question}</p>
         <button >
           {open ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </div>
-      <div className={!open? "max-h-0 opacity-0 mt-0 transition-all duration-200" : "overflow-hidden transition-all duration-500 max-h-full opacity-100 mt-2 "}>
-      <div className="bg-[#4b5563] border  border-slate-600 bg-opacity-20 my-5 p-5 rounded-xl drop-shadow-lg mt-4">
+      <div className={!open? "absolute max-h-0 opacity-0 mt-[-50] transition-all duration-200" : "overflow-hidden transition-all duration-500 max-h-full opacity-100 mt-4"}>
+      <div className="bg-[#4b5563] border  border-slate-600 bg-opacity-20 my-5 p-5 rounded-xl drop-shadow-lg">
       <p>"{answer}"</p>
       </div>
       </div>
