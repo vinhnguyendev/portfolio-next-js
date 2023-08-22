41d3cb491c55a6b2cf3c5c64e45bd8a532b0fdf1
TKHAN VIN NHUIEN <gatorfl2014@gmail.com>
2023-08-22 15:19:35 -0400

    Change mobile nav functionality

--- components/layout/project-detail-navbar.js
+++ components/layout/project-detail-navbar.js
@@ -4,10 +4,10 @@ import { IoChevronBack } from "react-icons/io5";
 export default function ProjectDetailNavbar(props) {
   return (
     <div className="flex flex-col h-20 bg-gradient-to-r from-from-current to-transparent">
-      <button className="my-auto px-5" onClick={props.handleGoBack}>
-        <div className="flex gap-4">
-          <IoChevronBack className="my-auto" size={40} />
-          <span className="my-auto text-2xl md:text-4xl lg:text-4xl ">Back</span>
+      <button className="my-auto px-4" onClick={props.handleGoBack}>
+        <div className="flex gap-1 lg:gap-4">
+          <IoChevronBack className="my-auto" size={20} />
+          <span className="my-auto text-xl ">Back</span>
         </div>
       </button>
     </div>
